3af88d3c4ec06b334dce7a4d58d108fe3349d0e1
Nyenty Ayuk <ayuknyenty15@gmail.com>
2021-10-04 03:55:18 +0000

    small name generator

---
+++ Programs/bandname.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+cName =input("What's the name of the city you live in?\n")
+pName =input("What's the name of your pet?\n")
+print("Your band name could be " +cName+' '+pName+ ' sounds pretty cool to me!')
